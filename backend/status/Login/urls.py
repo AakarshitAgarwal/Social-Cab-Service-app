8895b4373d3d1b23aba7c6b99dca8d9a32b155e0
Shreyansh Sharma <47782066+Shrey29ansh@users.noreply.github.com>
2020-05-11 21:37:29 +0530

    Update urls.py

--- backend/status/Login/urls.py
+++ backend/status/Login/urls.py
@@ -13,3 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf.urls import url
+from .views import TokenCreateView
+
+urlpatterns = [
+    url(r'^$', TokenCreateView.as_view(), name='account-create'),
+]
