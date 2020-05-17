172c204b4cc850cd5fa05a23d4febe2029697925
Shreyansh Sharma <47782066+Shrey29ansh@users.noreply.github.com>
2020-05-17 15:49:28 +0530

    Revert "trip api/ pep8"
    This reverts commit b5bfadf1c44356df141183aa0dd84895ffdaf407.

--- backend/status/register/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.UserCreate.as_view(), name='account-create'),
-    url(r'^Edit/(?P<username>.+)/$', views.UserDetail.as_view()),
-]
